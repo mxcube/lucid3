0a1a4df842b7833154bcccab901b480651f5dbf5
Jordi Andreu <jandreu@cells.es>
2020-03-27 15:45:42 +0100

    Add recipe for conda package

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # 	-*- coding: utf-8 -*-
 
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
+
+requirements = ['setuptools', 'python']
 
 setup(name='lucid3',
         version='1.0',
         description='ESRF Target Detection Software',
         author='Sogeti High Tech',
         packages=['lucid3'],
+        install_requires=requirements,
+        python_requires='>=2.7',
      )
